d44617ba27b4c2d4362e3d29b3554a745a2d7e80
Cristi Zanfir <zanfircristian92@gmail.com>
2018-02-11 15:31:22 +0000

    Added chef and recipe model with TDD

---
+++ app/models/chef.rb
@@ -0,0 +1,11 @@
+class Chef < ApplicationRecord
+  validates :chefname, presence: true, length: { maximum: 30 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+                    
+  before_save { self.email = email.downcase }
+  has_many :recipes
+end
