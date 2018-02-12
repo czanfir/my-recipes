085aca94b4f53da8b60e676d3c4552a40b271853
Cristi Zanfir <zanfircristian92@gmail.com>
2018-02-12 19:24:48 +0000

    Add edit action with TDD approach

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -9,5 +9,5 @@ class Chef < ApplicationRecord
   before_save { self.email = email.downcase }
   has_many :recipes
   has_secure_password
-  validates :password, presence: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
 end
