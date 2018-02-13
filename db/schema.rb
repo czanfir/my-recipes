88c6d02d2f5c259734119789ab200a17629433c6
Cristi Zanfir <zanfircristian92@gmail.com>
2018-02-13 18:37:19 +0000

    Add admin user functionality with tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212181339) do
+ActiveRecord::Schema.define(version: 20180213182216) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "recipes", force: :cascade do |t|
--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class ChefsEditTest < ActionDispatch::IntegrationTest
   def setup
-   @chef = Chef.create!(chefname: "cristi", email: "cristi@example.com",
+    @chef = Chef.create!(chefname: "cristi", email: "cristi@example.com",
                           password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "john", email: "john@example.com",
+                    password: "password", password_confirmation: "password")
+    @admin_user = Chef.create!(chefname: "john1", email: "john1@example.com",
+                          password: "password", password_confirmation: "password", admin: true) 
   end
   
   test "reject an invalid edit" do
@@ -37,4 +41,30 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     assert_match "cristi1", @chef.chefname
     assert_match "cristi1@example.com", @chef.email
   end
+  
+  test "accept edit attempt by admin user" do
+    sign_in_as(@admin_user, "password")
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: "cristi3", 
+                                  email: "cristi3@example.com" } }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "cristi3", @chef.chefname
+    assert_match "cristi3@example.com", @chef.email
+  end
+  
+  test "redirect edit attempt by another non-admin user" do
+    sign_in_as(@chef2, "password")
+    updated_name = "joe"
+    updated_email = "joe@example.com"
+    patch chef_path(@chef), params: { chef: { chefname: updated_name, 
+                                  email: updated_email } }
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "cristi", @chef.chefname
+    assert_match "cristi@example.com", @chef.email
+  end  
 end
