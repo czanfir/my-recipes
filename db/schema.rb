bce67ef52ec92bf346554948cc26ac60e12d93d3
Cristi Zanfir <zanfircristian92@gmail.com>
2018-02-14 09:12:13 +0000

    Add comments and all required associations with chefs and recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214084130) do
+ActiveRecord::Schema.define(version: 20180214084951) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "chef_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
