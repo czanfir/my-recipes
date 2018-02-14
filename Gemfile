2a213d5f28df5492419e0bbcbfac29602b675e08
Cristi Zanfir <zanfircristian92@gmail.com>
2018-02-14 17:28:27 +0000

    Make app production ready. Install and configure redis for heroku

--- Gemfile
+++ Gemfile
@@ -59,4 +59,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :production do
   gem 'pg', '~> 0.21'
+  gem 'redis'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://rormyrecipes.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://rormyrecipes.herokuapp.com/', 'http://rormyrecipes.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # config.web_socket_server_url = "wss://rormyrecipes.herokuapp.com/cable" 
 end
