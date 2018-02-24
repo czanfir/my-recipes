fb7792c25ed5f7b307484e6849cde76f6248070f
Cristi Zanfir <zanfircristian92@gmail.com>
2018-02-24 16:10:25 +0000

    Make chefchat realtime

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :require_user
   
   def create
     @message = Message.new(message_params)
     @message.chef = current_chef
     if @message.save
-      redirect_to chat_path
+      ActionCable.server.broadcast 'chatroom_channel', 
+                                    message: render_message(@message), 
+                                    chef: @message.chef.chefname
     else
       render 'chatrooms/show'
     end
@@ -14,5 +17,9 @@ class MessagesController < ApplicationController
   
   def message_params
     params.require(:message).permit(:content)
+  end
+  
+  def render_message(message)
+    render(partial: 'message', locals: { message: message } )
   end
 end
